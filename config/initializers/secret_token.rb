39599e47213bbeb4735efa52aa0c38cae71f9744
Angel Villanueva <avillanueva@sinapse.es>
2013-05-13 11:05:48 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebLlorens::Application.config.secret_token = '7434420d4b261fdd0f47cb4e500e4a0d12aa5c7e785a62757dd1f5bdfecc9abe56d2c65099d518b7ac62bab712b83677e5149f792fc73c12ce286dc86164d713'

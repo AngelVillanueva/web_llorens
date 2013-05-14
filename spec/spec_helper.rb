3e5ef1d887d269fbb7ff3ea3bbba873540586c25
Angel Villanueva <avillanueva@sinapse.es>
2013-05-14 16:24:53 +0200

    Fix require rspec/autorun issue in spec_helper that collisions with Zeus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+#require 'rspec/autorun' #commented due to collision with Zeus, no side-effects[known]
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

c7fc6a0cc59f699088e0c84d73b23da4fb9c6ca4
Angel Villanueva <avillanueva@sinapse.es>
2013-05-22 19:34:59 +0200

    API tests marked as 'pending'

--- spec/controllers/api_spec.rb
+++ spec/controllers/api_spec.rb
@@ -5,17 +5,19 @@ describe Api::V1::ExpedientesController do
   
   describe "when the allowed attributes are sent" do
     it "should return successful response" do
-      request.accept = "application/json"
-      json = { format: 'json', expediente: { identificador: "IM1234" } }
-      post :create, json
-      response.should be_success
+      pending
+      # request.accept = "application/json"
+      # json = { format: 'json', expediente: { identificador: "IM1234" } }
+      # post :create, json
+      # response.should be_success
     end
   end
   describe "when not allowed attributes are sent" do
     it "should return successful response" do
-      request.accept = "application/json"
-      json = { format: 'json', expediente: { identificadores: "IM1234" } }
-      expect{ post :create, json}.to raise_error(ActionController::UnpermittedParameters)
+      pending
+      # request.accept = "application/json"
+      # json = { format: 'json', expediente: { identificadores: "IM1234" } }
+      # expect{ post :create, json}.to raise_error(ActionController::UnpermittedParameters)
     end
   end
 end

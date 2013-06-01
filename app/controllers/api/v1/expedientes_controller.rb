76699f2c8a3f8967bca5d348669328af85bd0283
Angel Villanueva <avillanueva@sinapse.es>
2013-06-01 13:59:20 +0200

    API create tested (1 register)

--- app/controllers/api/v1/expedientes_controller.rb
+++ app/controllers/api/v1/expedientes_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::ExpedientesController < ApplicationController
   skip_before_filter :authenticate_usuario!
   respond_to :json
-  expose(:expediente)
+  expose( :expediente )
+  expose( :expediente_type ) { params[:expediente][:type].constantize }
 
   def create
-    expediente = Expediente.new(expediente_params)
+    expediente = expediente_type.new(expediente_params)
     if expediente.save
       respond_with(expediente)
     else
@@ -14,8 +15,9 @@ class Api::V1::ExpedientesController < ApplicationController
 
   private
   def expediente_params
+    params[:expediente].delete :type # to avoid Mass Assignment Error [:type is reserved]
     params
-      .require(:expediente)
-      .permit(:identificador, :matricula, :bastidor, :comprador, :vendedor, :marca, :modelo, :fecha_alta, :fecha_entra_trafico, :fecha_facturacion, :organizacion_id, :type)
+      .require( :expediente )
+      .permit( :identificador, :matricula, :bastidor, :comprador, :vendedor, :marca, :modelo, :fecha_alta, :fecha_entra_trafico, :fecha_facturacion, :organizacion_id, :observaciones )
   end
 end
--- spec/controllers/api_spec.rb
+++ spec/controllers/api_spec.rb
@@ -5,19 +5,39 @@ describe Api::V1::ExpedientesController do
   
   describe "when the allowed attributes are sent" do
     it "should return successful response" do
-      pending
-      # request.accept = "application/json"
-      # json = { format: 'json', expediente: { identificador: "IM1234" } }
-      # post :create, json
-      # response.should be_success
+      request.accept = "application/json"
+      json = { format: 'json', expediente: mock_expediente( Matriculacion ) }
+      post :create, json
+      response.should be_success
+      Expediente.count.should eql 1
+      Matriculacion.count.should eql 1
     end
   end
   describe "when not allowed attributes are sent" do
     it "should return successful response" do
-      pending
-      # request.accept = "application/json"
-      # json = { format: 'json', expediente: { identificadores: "IM1234" } }
-      # expect{ post :create, json}.to raise_error(ActionController::UnpermittedParameters)
+      request.accept = "application/json"
+      json = { format: 'json', expediente: { type: "Transferencia", identificadores: "IM1234" } }
+      expect{ post :create, json}.to raise_error(ActionController::UnpermittedParameters)
+      Expediente.count.should eql 0
     end
   end
 end
+
+private
+  def mock_expediente kind
+    expediente = {}
+    expediente[:type] = kind.to_s.camelize
+    expediente[:identificador] = "AAA"
+    expediente[:bastidor] = "AAA"
+    expediente[:matricula] = "AAA"
+    expediente[:comprador] = "AAA"
+    expediente[:vendedor] = "AAA"
+    expediente[:fecha_entra_trafico] = 5.days.ago.to_date
+    expediente[:fecha_alta] = 3.days.ago.to_date
+    expediente[:fecha_facturacion] = 1.day.ago.to_date
+    expediente[:marca] = "AAA"
+    expediente[:modelo] = "AAA"
+    expediente[:organizacion_id] = 1
+    expediente[:observaciones] = "AAA"
+    expediente
+  end

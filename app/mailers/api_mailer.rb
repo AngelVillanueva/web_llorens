db03fc1089c8eb302f97d06aa85dd6586dd9a1e5
Angel Villanueva <avillanueva@sinapse.es>
2013-09-30 19:06:21 +0200

    ApiMailer to jbrugada in Production

--- app/mailers/api_mailer.rb
+++ app/mailers/api_mailer.rb
@@ -1,6 +1,6 @@
 class ApiMailer < ActionMailer::Base
   if Rails.env.production?
-    recipients = ["info@sinapse.es"]
+    recipients = ["info@sinapse.es", "jbrugada@gestoriallorens.com"]
   else
     recipients = ["info@sinapse.es"]
   end

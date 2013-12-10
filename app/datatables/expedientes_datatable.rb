bf7e4ab58842712209cceb67c6bb54202e242481
Angel Villanueva <avillanueva@sinapse.es>
2013-12-10 16:59:26 +0100

    dataTables global search fixed

--- app/datatables/expedientes_datatable.rb
+++ app/datatables/expedientes_datatable.rb
@@ -6,6 +6,7 @@ class ExpedientesDatatable
     @type = type
     @current_ability = current_ability
     @columns = type_columns
+    @global_search_columns = global_search_columns
   end
 
   def as_json(options = {})
@@ -38,10 +39,15 @@ class ExpedientesDatatable
   end
 
   def fetch_expedientes
+    # fetch expedientes on page load
     expedientes = @type.unscoped.includes(:cliente).accessible_by( @current_ability ).page( page ).per( per_page )
+    # if global search refine results
     if params[:sSearch].present?
-      expedientes = expedientes.where("bastidor like :search or comprador like :search or modelo like :search", search: "%#{params[:sSearch]}%")
+      expedientes = global_search expedientes
     end
+    # if column search refine results
+    column_search expedientes
+    # sort if requested
     if params[:iSortCol_0].present?
       expedientes = expedientes.order("#{sort_column} #{sort_direction}")
     end
@@ -64,10 +70,31 @@ class ExpedientesDatatable
     params[:sSortDir_0] == "desc" ? "desc" : "asc"
   end
 
+  def global_search expedientes
+    searched = params[:sSearch]
+    searching = @global_search_columns.join(" ilike :search or ")
+    searching << " ilike :search"
+    expedientes = expedientes.where(searching, search: "%#{searched}%")
+  end
+
+  def column_search expedientes
+    expedientes
+  end
+
   def type_columns
     case @type.to_s
     when "Matriculacion"
       columns = %w[clientes.nombre matricula bastidor comprador modelo fecha_alta fecha_sale_trafico has_documentos]
+    when "Transferencia"
+      columns = %w[clientes.nombre]
+    end
+    columns
+  end
+
+  def global_search_columns
+    case @type.to_s
+    when "Matriculacion"
+      columns = %w[clientes.nombre matricula bastidor comprador modelo]
     when "Transferencia"
       columns = %w[clientes.nombre]
     end

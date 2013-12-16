2dd800b11b9ad9299349b96dbd5fd55afe364405
Angel Villanueva <avillanueva@sinapse.es>
2013-12-16 16:29:10 +0100

    Fix production default_url for host (issue sending new_informe emails)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,5 @@ WebLlorens::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { :host => 'gestoriallorens.com' }
 end

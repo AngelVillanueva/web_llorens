c479d7202e34450f9dc4597d9068ce00a265d230
Angel Villanueva <avillanueva@sinapse.es>
2014-02-20 12:37:04 +0100

    Annotate StockVehicle

--- spec/models/stock_vehicle_spec.rb
+++ spec/models/stock_vehicle_spec.rb
@@ -2,11 +2,30 @@
 #
 # Table name: stock_vehicles
 #
-#  id         :integer          not null, primary key
-#  matricula  :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  cliente_id :integer
+#  id                           :integer          not null, primary key
+#  matricula                    :string(255)
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#  cliente_id                   :integer
+#  marca                        :string(255)
+#  modelo                       :string(255)
+#  vendido                      :boolean          default(FALSE)
+#  comprador                    :string(255)
+#  ft                           :boolean          default(FALSE)
+#  pc                           :boolean          default(FALSE)
+#  fecha_itv                    :date
+#  incidencia                   :text
+#  fecha_expediente_completo    :date
+#  fecha_documentacion_enviada  :date
+#  fecha_documentacion_recibida :date
+#  fecha_notificado_cliente     :date
+#  particular                   :boolean          default(FALSE)
+#  compra_venta                 :boolean          default(FALSE)
+#  fecha_envio_gestoria         :date
+#  baja_exportacion             :boolean          default(TRUE)
+#  fecha_entregado_david        :date
+#  fecha_envio_definitiva       :date
+#  observaciones                :text
 #
 
 require 'spec_helper'

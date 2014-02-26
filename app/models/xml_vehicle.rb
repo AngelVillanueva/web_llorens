c2d44ef7f51ef2fcdf851d87f1a4cf63a61a9b6e
Angel Villanueva <avillanueva@sinapse.es>
2014-02-26 10:00:13 +0100

    Refine XmlVehicle RailsAdmin views

--- app/models/xml_vehicle.rb
+++ app/models/xml_vehicle.rb
@@ -7,6 +7,22 @@ class XmlVehicle < ActiveRecord::Base
   rails_admin do
     navigation_label I18n.t( "REMARKETING")
     label I18n.t( "Athlon files")
+    list do
+      field :id
+      field :cliente
+      field :xml do
+        label I18n.t( "Xml filepath" )
+      end
+      field :created_at do
+        label I18n.t( "Created" )
+      end
+      field :updated_at do
+        label I18n.t( "Updated" )
+      end
+      field :processed do
+        label I18n.t( "Processed" )
+      end 
+    end
     edit do
       field :xml do
         label I18n.t( "XML - label" )

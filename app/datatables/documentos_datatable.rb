1ad31bf4c6cf1ad62721e064dbc2ee58aeb79165
javivilla <javi.villa9@gmail.com>
2015-09-23 20:32:18 +0200

    Datetime between rangos

--- app/datatables/documentos_datatable.rb
+++ app/datatables/documentos_datatable.rb
@@ -115,8 +115,8 @@ class DocumentosDatatable
         elsif column.include? "pdf_updated_at"
           lapse = searched.split("~")
           unless lapse[1].nil?
-            f1 = lapse[0].to_datetime
-            f2 = lapse[1].to_datetime
+            f1 = lapse[0].to_datetime.beginning_of_day
+            f2 = lapse[1].to_datetime.end_of_day
             documentos = documentos.where( "#{column} between :f1 and :f2", f1: f1, f2:f2 ) unless (searched.empty? || searched == "~")
           end 
         else

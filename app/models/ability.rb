b7149bed7319c6eb6c54e28fb97255071d958093
javivilla <javi.villa9@gmail.com>
2016-05-20 09:59:45 +0200

    bug permisos

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,11 +17,11 @@ class Ability
         can :manage, StockVehicle
     elsif usuario.has_cli_remarketing?
         can :manage, Expediente, cliente_id: 0
-        can :manage, Matriculacion, cliente_id: (usuario.check_permisos) ? usuario.permisos_matriculas : usuario.clientes
-        can :manage, Transferencia, cliente_id: (usuario.check_permisos) ? usuario.permisos_transferencias : usuario.clientes
-        can :manage, Justificante, cliente_id: (usuario.check_permisos) ? usuario.permisos_justificantes : usuario.clientes
-        can :manage, Mandato, cliente_id: (usuario.check_permisos) ? usuario.permisos_mandatos : usuario.clientes
-        can :manage, Informe, cliente_id: (usuario.check_permisos) ? usuario.permisos_informes : usuario.clientes
+        can :manage, Matriculacion, cliente_id: (usuario.check_permisos) ? usuario.permisos_matriculas : usuario.cliente_ids
+        can :manage, Transferencia, cliente_id: (usuario.check_permisos) ? usuario.permisos_transferencias : usuario.cliente_ids
+        can :manage, Justificante, cliente_id: (usuario.check_permisos) ? usuario.permisos_justificantes : usuario.cliente_ids
+        can :manage, Mandato, cliente_id: (usuario.check_permisos) ? usuario.permisos_mandatos : usuario.cliente_ids
+        can :manage, Informe, cliente_id: (usuario.check_permisos) ? usuario.permisos_informes : usuario.cliente_ids
         can :manage, Cliente, id: usuario.cliente_ids
         can :manage, StockVehicle, cliente: { :id => usuario.cliente_ids }
         can :manage, Documento
@@ -29,11 +29,11 @@ class Ability
     # Common users
     elsif usuario
         can :manage, Expediente, cliente_id: 0
-        can :manage, Matriculacion, cliente_id: (usuario.check_permisos) ? usuario.permisos_matriculas : usuario.clientes
-        can :manage, Transferencia, cliente_id: (usuario.check_permisos) ? usuario.permisos_transferencias : usuario.clientes
-        can :manage, Justificante, cliente_id: (usuario.check_permisos) ? usuario.permisos_justificantes : usuario.clientes
-        can :manage, Mandato, cliente_id: (usuario.check_permisos) ? usuario.permisos_mandatos : usuario.clientes
-        can :manage, Informe, cliente_id: (usuario.check_permisos) ? usuario.permisos_informes : usuario.clientes
+        can :manage, Matriculacion, cliente_id: (usuario.check_permisos) ? usuario.permisos_matriculas : usuario.cliente_ids
+        can :manage, Transferencia, cliente_id: (usuario.check_permisos) ? usuario.permisos_transferencias : usuario.cliente_ids
+        can :manage, Justificante, cliente_id: (usuario.check_permisos) ? usuario.permisos_justificantes : usuario.cliente_ids
+        can :manage, Mandato, cliente_id: (usuario.check_permisos) ? usuario.permisos_mandatos : usuario.cliente_ids
+        can :manage, Informe, cliente_id: (usuario.check_permisos) ? usuario.permisos_informes : usuario.cliente_ids
         can :manage, Cliente, id: usuario.cliente_ids
         can :manage, StockVehicle, cliente: { :id => usuario.cliente_ids }
     end

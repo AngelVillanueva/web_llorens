5f3240200a8c39954c422af771499c69ba33b0a8
javivilla <javi.villa9@gmail.com>
2016-11-02 11:05:50 +0100

    plantilla para mail incidencias

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,8 +12,8 @@ every 30.minutes do
   rake "pdfer:match_empty"
 end
 
-every 1.minutes do
-	runner "IncidenciasMailer.listado"
+every 3.minutes do
+	runner "IncidenciasMailer.delay.listado_incidencias"
 end
 
 set :output, {:error => '~/Escritorio/z.error.log', :standard => '~/Escritorio/z.standard.log'}

8e746eaa180f07c5ab3a12fed291df84c3fa13c5
javivilla <javi.villa9@gmail.com>
2016-11-03 18:24:43 +0100

    Enviar por mail solo incidencias NO resueltas

--- app/mailers/incidencias_mailer.rb
+++ app/mailers/incidencias_mailer.rb
@@ -5,7 +5,7 @@ class IncidenciasMailer < ActionMailer::Base
 
   def listado_incidencias
     recipients = Usuario.where("incidencias=true").map(&:email)
-    @incidencias = Transferencia.where("has_incidencia = ? AND fecha_alta < ?",true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
+    @incidencias = Transferencia.where("has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
     recipients.each do |email|
         mail to: email, subject: t( "Incidencias vehiculos" )
     end

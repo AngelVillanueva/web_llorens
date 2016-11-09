64142b39677090ddde7ae3e2644d3651a3f31e04
javivilla <javi.villa9@gmail.com>
2016-11-09 14:26:47 +0100

    arreglada consulta mail incidencias

--- app/mailers/incidencias_mailer.rb
+++ app/mailers/incidencias_mailer.rb
@@ -6,16 +6,15 @@ class IncidenciasMailer < ActionMailer::Base
   def listado_incidencias
     recipients = Usuario.where("incidencias=true and (email='javi.villa9@gmail.com' or email='javier.villanueva@netberry.es')").map(&:email)
     recipients.each do |email|
-        IncidenciasMailer.delay.send_incidencias(email)
+        IncidenciasMailer.send_incidencias(email)
     end
   end
 
   def send_incidencias r
-    id = Usuario.where("email=?",r).pluck(:id)
-    usuario = Usuario.find(id)
-    unless current_usuario.role?("employee") || current_usuario.role?("admin")
+    usuario = Usuario.find_by_email(r);
+    unless usuario.role?("employee") || usuario.role?("admin")
       clientes = usuario.norole? && usuario.organizacion.clientes || Cliente.all
-    @incidencias = Transferencia.where("clientes IN (?) AND has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",clientes,true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
+      @incidencias = Transferencia.where("cliente_id IN (?) AND has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",clientes,true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
     else
        @incidencias = Transferencia.where("has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
     end

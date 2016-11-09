ebc94473af58bcd891fb42d2618782a3be13d68e
javivilla <javi.villa9@gmail.com>
2016-11-09 13:05:26 +0100

    bug fix consulta clientes mail incidencias

--- app/mailers/incidencias_mailer.rb
+++ app/mailers/incidencias_mailer.rb
@@ -1,17 +1,17 @@
 class IncidenciasMailer < ActionMailer::Base
-  recipients = Usuario.where("incidencias=true and email='javi.villa9@gmail.com'").map(&:email)
+  recipients = Usuario.where("incidencias=true and email='javier.villanueva@netberry.es'").map(&:email)
   default to: Proc.new { recipients },
     from: "no-reply@gestoriallorens.com"
 
   def listado_incidencias
-    recipients = Usuario.where("incidencias=true and email='javi.villa9@gmail.com'").map(&:email)
+    recipients = Usuario.where("incidencias=true and email='javier.villanueva@netberry.es'").map(&:email)
     recipients.each do |email|
         IncidenciasMailer.delay.send_incidencias(email)
     end
   end
 
   def send_incidencias r
-     usuario = Usuario.where("email='{r}'").limit(1).pluck(:id)
+     usuario = Usuario.where("email=?",r).limit(1).pluck(:id)
      clientes = Usuario.find(usuario).clientes.pluck(:id)
      @incidencias = Transferencia.where("clientes IN (?) AND has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",clientes,true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
      mail to: r, subject: t( "Incidencias vehiculos" )

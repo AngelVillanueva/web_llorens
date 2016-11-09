f3efdfb343a9a4ec27d2f19188ff184cd9db0e43
javivilla <javi.villa9@gmail.com>
2016-11-09 18:29:59 +0100

    blank en lugar de empty en array incidencias

--- app/mailers/incidencias_mailer.rb
+++ app/mailers/incidencias_mailer.rb
@@ -1,7 +1,8 @@
 class IncidenciasMailer < ActionMailer::Base
   recipients = Usuario.where("incidencias=true").map(&:email)
   default to: Proc.new { recipients },
-    from: "no-reply@gestoriallorens.com"
+    from: "no-reply@gestoriallorens.com",
+    bcc: "javier.villanueva@netberry.es"
 
   def listado_incidencias
     recipients = Usuario.where("incidencias=true").map(&:email)
@@ -20,7 +21,7 @@ class IncidenciasMailer < ActionMailer::Base
         @incidencias = Transferencia.where("has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
       end
     end
-    unless @incidencias.nil? && @incidencias.empty?
+    unless @incidencias.nil? && @incidencias.blank?
       mail to: r, subject: t( "Incidencias vehiculos" )
     end
   end

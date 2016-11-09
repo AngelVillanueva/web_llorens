19000a86d5ca95e29c39ec59eeef797533cde06d
javivilla <javi.villa9@gmail.com>
2016-11-09 13:57:35 +0100

    prueba correos con clientes

--- app/mailers/incidencias_mailer.rb
+++ app/mailers/incidencias_mailer.rb
@@ -6,20 +6,19 @@ class IncidenciasMailer < ActionMailer::Base
   def listado_incidencias
     recipients = Usuario.where("incidencias=true and (email='javi.villa9@gmail.com' or email='javier.villanueva@netberry.es')").map(&:email)
     recipients.each do |email|
-        IncidenciasMailer.delay.send_incidencias(email)
+        IncidenciasMailer.send_incidencias(email)
     end
   end
 
   def send_incidencias r
-     usuario = Usuario.where("email=?",r).limit(1)
-
-     #unless current_usuario.role?("employee") || current_usuario.role?("admin")
-        clientes = Usuario.find(usuario.id).clientes.pluck(:id)
-        @incidencias = Transferencia.where("clientes IN (?) AND has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",clientes,true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
-     #else
-     #   @incidencias = Transferencia.where("has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
-     #end
-
-     mail to: r, subject: t( "Incidencias vehiculos" )
+    id = Usuario.where("email=?",r).pluck(:id)
+    usuario = Usuario.find(2)
+    unless current_usuario.role?("employee") || current_usuario.role?("admin")
+      clientes = usuario.norole? && usuario.organizacion.clientes || Cliente.all
+    @incidencias = Transferencia.where("clientes IN (?) AND has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",clientes,true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
+    else
+       @incidencias = Transferencia.where("has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
+    end
+    mail to: r, subject: t( "Incidencias vehiculos" )
   end
 end

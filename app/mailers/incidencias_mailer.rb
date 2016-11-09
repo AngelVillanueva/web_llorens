03abc587ff9c18e8ff77475208d6028b15a0939a
javivilla <javi.villa9@gmail.com>
2016-11-09 14:03:59 +0100

    bug fix cola correos

--- app/mailers/incidencias_mailer.rb
+++ app/mailers/incidencias_mailer.rb
@@ -6,13 +6,13 @@ class IncidenciasMailer < ActionMailer::Base
   def listado_incidencias
     recipients = Usuario.where("incidencias=true and (email='javi.villa9@gmail.com' or email='javier.villanueva@netberry.es')").map(&:email)
     recipients.each do |email|
-        IncidenciasMailer.send_incidencias(email)
+        IncidenciasMailer.delay.send_incidencias(email)
     end
   end
 
   def send_incidencias r
     id = Usuario.where("email=?",r).pluck(:id)
-    usuario = Usuario.find(2)
+    usuario = Usuario.find(id)
     unless current_usuario.role?("employee") || current_usuario.role?("admin")
       clientes = usuario.norole? && usuario.organizacion.clientes || Cliente.all
     @incidencias = Transferencia.where("clientes IN (?) AND has_incidencia = ? AND fecha_alta < ? AND fecha_resolucion_incidencia IS NULL",clientes,true,Date.today - 5).order("cliente_id asc, fecha_alta asc");
